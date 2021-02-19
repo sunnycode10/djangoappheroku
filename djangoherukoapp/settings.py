c07a7510cf5981b237e41103dafdc5cd5f9e78d6
sunday emmanuel <emmanuel.sunday@diamondacresoftware.com>
2021-02-19 02:10:55 +0100

    initial commit

--- djangoherukoapp/settings.py
+++ djangoherukoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#1mqy*fvdn88m@wr4&p(__)1x065h@3c)vlzg7ex8lpp#49fo4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapsunnycode.herokuapp.com']
+ALLOWED_HOSTS = ['sunnycodedeploy.herokuapp.com']
 
 
 # Application definition
